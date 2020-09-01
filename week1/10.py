54fd40e6a78bbc9f43d78977c0fcc4516b074a8b
EetuHei <t8heee01@students.oamk.fi>
2020-09-01 11:58:33 +0300

    exercise 10 done

---
+++ week1/10.py
@@ -0,0 +1,19 @@
+# Sort following NumPy array
+import numpy as np
+arr = np.array([[34, 43, 73], [82, 22, 12], [53, 94, 66]])
+print("original array")
+print(arr)
+
+sortArrByRow = arr[:, arr[1, :].argsort()]
+print("sorting original array by second row")
+# [[73 43 34]
+#  [12 22 82]
+#  [66 94 53]]
+print(sortArrByRow)
+
+sortArrByColumn = arr[arr[:, 1].argsort()]
+print("sorting array by second column")
+# [[82 22 12]
+#  [34 43 73]
+#  [53 94 66]]
+print(sortArrByColumn)
