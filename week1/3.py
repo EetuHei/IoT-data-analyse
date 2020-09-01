c14dade787e189bb1c43d18c5c2aa2ba9ccd4b8c
EetuHei <t8heee01@students.oamk.fi>
2020-09-01 11:57:43 +0300

    exercise 3 done

---
+++ week1/3.py
@@ -0,0 +1,6 @@
+# Write a NumPy program to create an array with the values 1, 7, 13, 105 and determine the size of the memory occupied by the array. 
+import numpy as np
+x = np.array([1, 2, 3, 4])
+
+# 16 bytes
+print("%d bytes" % (x.size * x.itemsize))
