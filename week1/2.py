420320348f9184b86033d6cddf1714d54786ebf0
EetuHei <t8heee01@students.oamk.fi>
2020-09-02 13:44:25 +0300

    cleaned code

--- week1/2.py
+++ week1/2.py
@@ -2,20 +2,15 @@
 import numpy as np
 x = np.array([1, 2, 3, 4])
 
-# Original array:
-# [1 2 3 4]
-# Test if none of the elements of the said array is zero:
-# True
-# Original array:
-# [0 1 2 3]
-# Test if none of the elements of the said array is zero:
-# False
 print("Original array:")
 print(x)
 print("Test if none of the elements of the said array is zero:")
+# True
 print(np.all(x))
+
 x = np.array([0, 1, 2, 3])
 print("Original array:")
 print(x)
 print("Test if none of the elements of the said array is zero:")
+# False
 print(np.all(x))
